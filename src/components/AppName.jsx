2a459c62fbd0546d3b7bed653795469366c0516e
mohithsaran <mohithsaran.kanamarlapudi@gmail.com>
2024-03-24 19:38:38 +0530

    Todo React App done

---
+++ src/components/AppName.jsx
@@ -0,0 +1,13 @@
+import styles from "./AppName.module.css"
+
+const AppName = () => {
+  return (
+    <>
+    <center>
+      <h1 className={styles.todoHeading}>TODO React App</h1>
+      </center>
+    </>
+  );
+}
+
+export default AppName;
